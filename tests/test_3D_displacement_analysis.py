added function to correct for apparent radial displacement due to membrane rotation.

tests/test_3D_displacement_analysis.py
@@ -9,6 +9,8 @@ import smu, awg, dpt
 from utils import settings, analyses, plotting, empirical
 
 
+
+
 if __name__ == "__main__":
     """
     NOTES:
@@ -16,8 +18,8 @@ if __name__ == "__main__":
     """
 
     # THESE ARE THE ONLY SETTINGS YOU SHOULD CHANGE
-    TEST_CONFIG = '0112025_W11-B1_C9-0pT'
-    TIDS = [1, 4, 5, 6, 7]  # np.arange(3, 15)  # [56, 62, 63] or np.arange(30, 70) or np.flip(np.arange(30, 70))
+    TEST_CONFIG = '01082025_W5-D1_C9-0pT'
+    TIDS = [2]  # np.arange(3, 15)  # [56, 62, 63] or np.arange(30, 70) or np.flip(np.arange(30, 70))
     IV_ACDC = 'DC'  # 'AC' or 'DC'
     ANIMATE_FRAMES = None  # None: defer to test_settings; to override test_settings: np.arange(20, 115)
     # -
@@ -32,16 +34,25 @@ if __name__ == "__main__":
     # -
     # ANALYSES
     XYM = ['g']  # ['g', 'm']: use sub-pixel or discrete in-plane localization
-    SECOND_PASS = False  # True False
-    EXPORT_NET_D0ZR, AVG_MAX_N = True, 4  # True: export dfd0 to special directory
+    SECOND_PASS = True  # True False
+    EXPORT_NET_D0ZR, AVG_MAX_N = False, 5  # True: export dfd0 to special directory
     # -
     # ALTERNATIVE IS TO USE INITIAL COORDS
     EXPORT_INITIAL_COORDS = False  # False True
-    D0F_IS_TID = 1  # ONLY USED IF DICT_TID{}_SETTINGS.XLSX IS NOT FOUND
+    D0F_IS_TID = 2  # ONLY USED IF DICT_TID{}_SETTINGS.XLSX IS NOT FOUND
     DROP_PIDS = []  # []: remove bad particles from ALL coords
     # -
     # ONLY USED IF DICT_TID{}_SETTINGS.XLSX IS NOT FOUND **AND** IV_ACDC == 'DC'
-    START_FRAME, END_FRAMES = (0, 6), (100, 120)  # (a<x<b; NOT: a<=x<=b) only used if test_settings.xlsx not found
+    START_FRAME, END_FRAMES = (50, 75), (170, 200)  # (a<x<b; NOT: a<=x<=b) only used if test_settings.xlsx not found
+
+    # SURFACE PROFILE
+    # used for correcting apparent radial displacement
+    # Note: I should add these variables to DICT_SETTINGS
+    subset = 'right'
+    include_hole = True
+    shift_r = 0
+    shift_z = 0
+    scale_z = 1
 
 
     # ------------------------------------------------------------------------------------------------------------------
@@ -116,6 +127,7 @@ if __name__ == "__main__":
         # ---
         DICT_SETTINGS, DICT_TEST = settings.settings_handler(settings_handler_dict=SETTINGS_HANDLER_DICT)
         # ---
+        """
         if not os.path.exists(join(SAVE_COORDS, FN_MERGED)):
             PRE_PROCESS_COORDS = True  # If you change andor_keithley_delay time, you must pre-process coords.
             PRE_PROCESS_IV = True  # Only needs to be run once per-tid; not dependent on synchronization timing settings.
@@ -124,6 +136,7 @@ if __name__ == "__main__":
             PRE_PROCESS_COORDS = False  # If you change andor_keithley_delay time, you must pre-process coords.
             PRE_PROCESS_IV = False  # Only needs to be run once per-tid; not dependent on synchronization timing settings.
             MERGE_COORDS_AND_VOLTAGE = False
+        """
         # PRE-PROCESSING
         # ---
         if PRE_PROCESS_COORDS or PRE_PROCESS_IV or MERGE_COORDS_AND_VOLTAGE:
@@ -156,6 +169,31 @@ if __name__ == "__main__":
                     # calculate rolling min (window size = images per voltage level): 'dz_lock_in'
                     DF = dpt.calculate_lock_in_rolling_min(
                         df=DF, pcols=['dz', 'd0z'], settings=DICT_SETTINGS, test_settings=DICT_TEST)
+
+                    # calculate membrane-rotation-corrected radial displacement
+                    # NOTE: the correction shouldn't be applied until the membrane zips in
+                    # So, this needs to be applied in conjunction with get_zipping_interface()
+                    # Which is not an easy task.
+                    """
+                    dict_surface_profilometry = empirical.get_surface_profile_dict(
+                        DICT_SETTINGS, subset, include_hole, shift_r, shift_z, scale_z)
+                    for poly_deg in [5, 3]:
+                        func_apparent_r_displacement = empirical.calculate_apparent_radial_displacement_due_to_rotation(
+                            surf_r=dict_surface_profilometry['r'],
+                            surf_z=dict_surface_profilometry['z'],
+                            poly_deg=poly_deg,
+                            membrane_thickness=DICT_SETTINGS['membrane_thickness'],
+                            z_clip=-0.25,
+                            path_save=SAVE_SETTINGS,
+                        )
+                        
+                        dfd0_corr = dfd0.copy()
+                        dfd0_corr['dr_corr'] = func_apparent_r_displacement(dfd0_corr[pr])
+                        dfd0_corr['dr_mean_corr'] = dfd0_corr['dr_mean'] + dfd0_corr['dr_corr']
+                        dfd0_corr = dfd0_corr.drop(columns=['id', 'xg', 'yg', 'rg_mean_f', 'z_mean_f',
+                                                            'r_strain', 'drdz', 'start_frame_i', 'start_frame_f',
+                                                            'end_frame_i', 'end_frame_f', 'average_max_n_positions',])
+                    """
                 # export transformed dataframe
                 # for reference: physical + pixel coordinates
                 # DF.to_excel(join(SAVE_COORDS_W_PIXELS, FN_COORDS_SAVE), index=True)